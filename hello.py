a0af8001d89fddca86e1f0e2dd271f1aa5287f07
LtvnSergey <ltvn.sergey@gmail.com>
2022-11-28 20:30:21 +0300

    Add Gradient Workflow

--- hello.py
+++ hello.py
@@ -1,11 +1,3 @@
-#save this file as hello.py in your repo
-import tensorflow as tf
 
-# Simple hello world using TensorFlow
-hello = tf.constant('Hello, TensorFlow!')
 
-# Start tf session
-sess = tf.Session()
-
-# Run the op
-print(sess.run(hello))
+print('hello')
